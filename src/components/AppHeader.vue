601622a6f2309be7ae18e5be9663e5bd48ceb403
MattiaPaiano91 <paianomattia1@gmail.com>
2024-01-25 17:03:13 +0100

    -completato header milestone 4

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,43 +1,70 @@
 <script>
-import { data } from '../store.js';
-import axios from 'axios';
+import { data } from "../store.js";
+import axios from "axios";
 export default {
-    data() {
-        return {
-            data
-        };
-    },
-    methods: {
-         
-    }
-}
+  data() {
+    return {
+      data,
+    };
+  },
+  methods: {},
+};
 </script>
-                  
 
 <template>
-    <header>
-        <form 
-        @submit.prevent="$emit('userSearch')"
-        action="">
-            <div class="input-group mb-3">
-                <input v-model="data.userSearch" type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Cerca</button>
-            </div>
+  <header class="p-3">
+    <div class="row">
+
+      <div class="col-6 d-flex align-items-center logo-box">
+          <h1 class="title">BOOLZAP</h1>
+      </div>
+          
+      <div class="col-6 d-flex align-items-center justify-content-end ">
+        <form class="h-75" @submit.prevent="$emit('userSearch')" action="">
+          <div class="input-group mb-3 d-flex align-items-center">
+            <input
+              v-model="data.userSearch"
+              type="text"
+              class="form-control "
+              placeholder="Cerca Serie tv o Film"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <button
+              class="btn btn-outline-secondary"
+              type="submit"
+              id="button-addon2"
+            >
+              Cerca
+            </button>
+          </div>
         </form>
-    </header>
+      </div>
+
+    </div>
+  </header>
 </template>
-<!-- <form 
-        @submit.prevent="$emit('userSearch')"
-        action="">
-            <input v-model="data.userSearch" type="text">
-            <button>Cerca</button>
-        </form> -->
+
 <style lang="scss" scoped>
-    @use "../assets/scss/main.scss" as *;
-    form{
-        width: 1200px;
-        margin: 0 auto;
+@use "../assets/scss/main.scss" as *;
+
+header {
+    height: 100px;
+    background-color: black;
+    .row{
+        height: 100%;
+        .logo-box{
+            height: 100%;
+            line-height: 100px;
+            .title{
+                color: red;
+                font-family: 'Bebas Neue', sans-serif;
+                margin: auto 0;
+            }
+        }
+      form {
+       width: 400px;
+      }
+    }
     }
 </style>
-       
-
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -33,33 +33,55 @@ export default {
 
 <template>
     <main>
-        <div v-for="(elem,i) in resultMovie" >
-            <ul>
+        <div class="card-container" v-for="(elem,i) in resultMovie" :key="i">
+            <div class="card">
+                <li><img :src="'https://image.tmdb.org/t/p/w342' + elem.poster_path" :alt="elem.original_title"></li>
+
+            </div>
+            <div class="card-information">
+                <ul>
                 <li>Title:{{ elem.title }}</li>
                 <li>Original title:{{ elem.original_title}}</li>
                 <li>
                     <img :src="rightFlag(elem.original_language)" :alt=elem.original_language>
                 </li>
                 <li>vote:{{ elem.vote_average}}</li>
-                <li><img :src="'https://image.tmdb.org/t/p/w342' + elem.poster_path" :alt="elem.original_title"></li>
             </ul>
+            </div>
         </div>
-        <div v-for="(series,j) in resultTV">
-            <ul>
+        <div class="card-container" v-for="(series,j) in resultTV" :key="j">
+         <div class="card">
+
+             <li><img :src="'https://image.tmdb.org/t/p/w342' + series.poster_path" :alt="series.original_name"></li>
+         </div>
+         <div  class="card-information">
+               <ul>
                 <li>SERIES Title:{{ series.name }}</li>
                 <li>Original title:{{ series.original_name}}</li>
                 <li>
                     <img :src="rightFlag(series.original_language)" :alt=series.original_language>
                 </li>
                 <li>vote:{{ series.vote_average}}</li>
-                <li><img :src="'https://image.tmdb.org/t/p/w342' + series.poster_path" :alt="series.original_name"></li>
             </ul>
+         </div>
         </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/main.scss" as *;
+
+main{
+    font-family: 'Bebas Neue', sans-serif;
+   .cardcontainer{
+        .card{
+
+        }
+        .card-information{
+            display: none;
+        }
+   }
+}
 </style>
 
            
